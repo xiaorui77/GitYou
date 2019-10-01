ebd1e70e5c55ca3a75895b808ff5bd0844382431
yongtao <cytcyt123@163.com>
2019-10-01 20:25:43 +0800

    feature: 获取仓库的所有Branch

--- repository/repository-service/src/main/java/com/gityou/repository/controller/BranchController.java
+++ repository/repository-service/src/main/java/com/gityou/repository/controller/BranchController.java
@@ -1,10 +1,14 @@
 package com.gityou.repository.controller;
 
 
+import com.gityou.repository.pojo.Branch;
 import com.gityou.repository.service.BranchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BranchController {
@@ -15,8 +19,12 @@ public class BranchController {
     /* 根据仓库查询branch
      * */
     @GetMapping("list")
-    public void queryList() {
-
+    public ResponseEntity<List<Branch>> queryList(Long id) {
+        List<Branch> result = branchService.queryList(id);
+        if (result == null)
+            return ResponseEntity.notFound().build();
+        else
+            return ResponseEntity.ok(result);
     }
 
 }// end
--- repository/repository-service/src/main/java/com/gityou/repository/service/BranchService.java
+++ repository/repository-service/src/main/java/com/gityou/repository/service/BranchService.java
@@ -2,11 +2,21 @@ package com.gityou.repository.service;
 
 
 import com.gityou.repository.mapper.BranchMapper;
+import com.gityou.repository.pojo.Branch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BranchService {
     @Autowired
     private BranchMapper branchMapper;
-}
+
+    public List<Branch> queryList(Long id) {
+        Branch record = new Branch();
+        record.setRepoId(id);
+        return branchMapper.select(record);
+    }
+
+}// end
